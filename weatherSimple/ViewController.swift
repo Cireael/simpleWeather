3d6fb04035dde9e9c5946af730b72bb68e753004
Cireael <oren.si@yandex.ru>
2023-05-08 22:28:42 +0300

    first

---
+++ weatherSimple/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  weatherSimple
+//
+//  Created by Julia on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var getWeatherButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getWeatherButton.addTarget(self, action: #selector(didTapGetWeatherButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapGetWeatherButton() {
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                DispatchQueue.main.async {
+                    self.tempLabel.text = "\(weather.currentWeather.temperature)"
+                }
+            } else {
+                print("Fail")
+            }
+        }
+        task.resume()
+    }
+
+
+}
+
